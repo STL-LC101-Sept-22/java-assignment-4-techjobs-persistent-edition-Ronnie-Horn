ee2a101fa5f203105f2b687784b2b46700aa856b
Ronnie Horn <ronniehorn@hotmail.com>
2023-03-17 23:40:25 -0500

    4 commit

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,9 +1,10 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+@Entity
 public class Employer extends AbstractEntity {
     @Size(max = 75, message = "Location name should not be longer than 75 characters")
     @NotBlank
