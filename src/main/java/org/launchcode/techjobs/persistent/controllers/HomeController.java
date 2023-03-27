751d8debcfbf683f4a5574c7330e5d5bcf51e254
Ronnie Horn <ronniehorn@hotmail.com>
2023-03-26 22:55:12 -0500

    Part 4 halfway done! commit

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -19,6 +22,8 @@ public class HomeController {
 
     @Autowired
         private EmployerRepository employerRepository;
+    @Autowired
+        private SkillRepository skillRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -26,13 +31,16 @@ public class HomeController {
         model.addAttribute("title", "My Jobs");
 
         return "index";
+
     }
 
     @GetMapping("add")
+
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
         model.addAttribute("employers",employerRepository.findAll());
+        model.addAttribute("skills",skillRepository.findAll());
         return "add";
     }
 
@@ -56,3 +64,14 @@ public class HomeController {
 
 
 }
+//    @GetMapping("view/{employerId}")
+//    public String displayViewEmployer(Model model, @PathVariable int employerId) {
+//
+//        Optional optEmployer = employerRepository.findById(employerId);
+//        if (optEmployer.isPresent()) {
+//            Employer employer = (Employer) optEmployer.get();
+//            model.addAttribute("employer", employer);
+//            return "employers/view";
+//        } else {
+//            return "redirect:../";
+//        }
